c4df952c1ac181c82e4162e09dcf0ac39daa0e10
pawan2210 <89751440+pawan2210@users.noreply.github.com>
2022-08-27 11:27:28 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Photo from './Photo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src ={Photo} alt = "not available"/>
+        <h2>Wlecome to my Website</h2>
+        <h3>MY name is Pawan</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <p>Contact me</p>
+        </div>
+    
+  )
+}
+
+export default Contact;
